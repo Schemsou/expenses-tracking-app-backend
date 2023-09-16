c9d89073a262ba383ae602936c64e250a0fc59a5
chemseddine.sediri <chemseddine.sediri@emploitic.com>
2023-09-17 00:16:25 +0100

    aggregation

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   const config = new DocumentBuilder()
-  .setTitle('Expenses Tracker API')
+    .setTitle('Expenses Tracker API')
   .setDescription('The expenses tracker api docummentation')
   .setVersion('1.0')
   .addTag('expenses')
   .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3333);
 }
 bootstrap();
