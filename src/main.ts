7f335316ba7882b77e7292e8d8d9d93206e92852
chemseddine.sediri <chemseddine.sediri@emploitic.com>
2023-09-16 16:52:11 +0100

    adding the api docs using nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+  const config = new DocumentBuilder()
+  .setTitle('Expenses Tracker API')
+  .setDescription('The expenses tracker api docummentation')
+  .setVersion('1.0')
+  .addTag('expenses')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3333);
 }
 bootstrap();
